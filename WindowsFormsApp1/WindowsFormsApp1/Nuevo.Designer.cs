43bbcd3108ea3218798c4cf5e09921a3cb055b9e
IsmaelMarin <65259018+IsmaelMarin@users.noreply.github.com>
2020-05-15 17:51:31 -0600

    Otro formulario

---
+++ WindowsFormsApp1/WindowsFormsApp1/Nuevo.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Nuevo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(190, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(464, 131);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hello";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Nuevo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Nuevo";
+            this.Text = "Nuevo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
